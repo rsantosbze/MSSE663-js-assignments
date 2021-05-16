f942388a3db18e47309221fcba8b061a77a09272
Rolando Santos <rjsaint@yahoo.com>
2021-05-16 06:51:38 -0600

    Week-2 submission

--- week2/arrays-and-loops.js
+++ week2/arrays-and-loops.js
@@ -1,6 +1,9 @@
 // #1 Take an array of numbers and make them strings
 function stringItUp(arr) {
-	// your code here
+  return arr.map((element) => {
+    return element.toString()
+  });
+
 }
 
 console.log(stringItUp([2, 5, 100]));
@@ -8,61 +11,72 @@ console.log(stringItUp([2, 5, 100]));
 
 // #2 Make an array of strings of the names
 function namesOnly(arr) {
-	// your code here
+  return arr.map((value) => {
+    return value.name;
+  });
+  
 }
 
 console.log(
-	namesOnly([
-		{
-			name: 'Angelina Jolie',
-			age: 80,
-		},
-		{
-			name: 'Eric Jones',
-			age: 2,
-		},
-		{
-			name: 'Paris Hilton',
-			age: 5,
-		},
-		{
-			name: 'Kayne West',
-			age: 16,
-		},
-		{
-			name: 'Bob Ziroll',
-			age: 100,
-		},
-	])
+  namesOnly([
+    {
+      name: 'Angelina Jolie',
+      age: 80,
+    },
+    {
+      name: 'Eric Jones',
+      age: 2,
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5,
+    },
+    {
+      name: 'Kayne West',
+      age: 16,
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100,
+    },
+  ])
 );
 // output: ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
 
 // #3 Filter the output array from #2.
 function oldOnesOnly(arr) {
-	// your code here
+
+  return arr
+    .filter((value) => {
+      return value.age >= 80;
+    })
+    .map((el) => {
+      return el.age;
+    });
+
 }
 console.log(
-	oldOnesOnly([
-		{
-			name: 'Angelina Jolie',
-			age: 80,
-		},
-		{
-			name: 'Eric Jones',
-			age: 2,
-		},
-		{
-			name: 'Paris Hilton',
-			age: 5,
-		},
-		{
-			name: 'Kayne West',
-			age: 16,
-		},
-		{
-			name: 'Bob Ziroll',
-			age: 100,
-		},
-	])
+  oldOnesOnly([
+    {
+      name: 'Angelina Jolie',
+      age: 80,
+    },
+    {
+      name: 'Eric Jones',
+      age: 2,
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5,
+    },
+    {
+      name: 'Kayne West',
+      age: 16,
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100,
+    },
+  ])
 );
 // output: [80, 100]
--- week2/object-literal.js
+++ week2/object-literal.js
@@ -3,6 +3,64 @@
  * #2 Give it 3 unique properties
  * #3 Give it at least 2 behaviours/methods of your choice
  * #4 Calls the object's method(s) and output a result of your choice to the console.
- * #5 Convert the object literal to an Class
- * #6 Then call its method and output to the console.
  */
+
+// ------- Option 1
+let monkey = {
+  type: 'spider',
+  color: 'dark brown',
+  weight: 20,
+  jumps: function () {
+    console.log(`The ${this.color} ${this.type} monkey has jumped!`);
+  },
+  ate: function (foodweight) {
+    return console.log (`The monkey's wieght is now ${
+      this.weight + foodweight
+    } lbs after eating ${foodweight} lbs.`);
+  },
+};
+monkey.jumps();
+monkey.ate(5);
+
+// ------- Option 2
+function Monkey(type, color, weight){
+    this.type = type;
+    this.color = color;
+    this.weight = weight;
+}
+
+Monkey.prototype.jumps = function(){
+    console.log(`The ${this.color} ${this.type} monkey has jumped!`);
+}
+
+Monkey.prototype.ate = function (foodweight) {
+    return console.log (`The monkey's weight is now ${
+      this.weight + foodweight
+    } lbs after eating ${foodweight} lbs.`);
+  };
+
+let myMonkey = new Monkey('howler', 'brown', 20);
+
+myMonkey.jumps();
+myMonkey.ate(10);
+
+// #5 Convert the object literal to an Class
+// #6 Then call its method and output to the console.
+
+class MonkeyClass {
+  constructor(type, color, weight) {
+    this.type = type;
+    this.color = color;
+    this.weight = weight;
+  }
+  howls() {
+    console.log(`The ${this.color} ${this.type} monkey is howling!`);
+  }
+  ate(foodweight) {
+    return console.log(`The monkey's weight is now ${this.weight + foodweight} lbs after eating ${foodweight} lbs.`);
+  }
+}
+let spiderMonkey = new MonkeyClass('spider', 'black', 20);
+spiderMonkey.howls();
+spiderMonkey.ate(5);
+
