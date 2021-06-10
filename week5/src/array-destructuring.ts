6c1d9fec566a988499ebaafa53021ecaa0659332
Rolando Santos <rjsaint@yahoo.com>
2021-06-10 08:17:08 -0600

    updated destructuring array

--- week5/src/array-destructuring.ts
+++ week5/src/array-destructuring.ts
@@ -1,12 +1,13 @@
 // #1 Variable swapping
 export const fruits = ['apple', 'banana'];
 
+export let [a, b] = fruits;
 
 // Currently, I would get the following:
 // console.log(a); // 'apple'
 // console.log(b); // 'banana'
 
-export const [b, a] = fruits;
+ [b, a] = fruits;
 
 // Assign variables using ES6 so that we get (note, you cannot just make a completely new array):
 console.log(a); // 'banana';
